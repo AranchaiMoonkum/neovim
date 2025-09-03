557250a85ae650f588ebb2cffd851b0b1a7d4a6d
Aranchai <moonkum.aranchai@gmail.com>
2025-09-03 19:09:35 +0700

    added gopls and formatted my code

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,24 +1,25 @@
 return {
-  "mason-org/mason-lspconfig.nvim",
-  dependencies = {
-    "mason-org/mason.nvim",
-    "neovim/nvim-lspconfig",
-  },
-  config = function()
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      automatic_enable = true,
-      ensure_installed = {
-        "lua_ls",
-        "angularls",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "ts_ls",
-        "jsonls",
-        "marksman",
-        "biome",
-      },
-    })
-  end,
+	"mason-org/mason-lspconfig.nvim",
+	dependencies = {
+		"mason-org/mason.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	config = function()
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			automatic_enable = true,
+			ensure_installed = {
+				"lua_ls",
+				"angularls",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"ts_ls",
+				"jsonls",
+				"marksman",
+				"biome",
+				"gopls",
+			},
+		})
+	end,
 }
