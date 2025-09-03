447d816bd1b994bb6646051ee405545380a68d4c
Aranchai <moonkum.aranchai@gmail.com>
2025-09-03 18:17:46 +0700

    added signature and source_name to columns in draw

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,24 +1,26 @@
 return {
-  "saghen/blink.cmp",
-  dependencies = "rafamadriz/friendly-snippets",
-  version = "1.*",
-  opts = {
-    keymap = { preset = "default" },
-    appearance = { nerd_font_variant = "mono" },
-    completion = {
-      documentation = { auto_show = true },
-      menu = {
-        draw = {
-          columns = {
-            { "label",     "label_description", gap = 1 },
-            { "kind" },
-          },
-        },
-      },
-    },
-    sources = {
-      default = { "lsp", "path", "snippets" },
-    },
-    fuzzy = { implementation = "prefer_rust" }
-  },
+	"saghen/blink.cmp",
+	dependencies = "rafamadriz/friendly-snippets",
+	version = "1.*",
+	opts = {
+		keymap = { preset = "default" },
+		appearance = { nerd_font_variant = "mono" },
+		completion = {
+			documentation = { auto_show = true },
+			menu = {
+				draw = {
+					columns = {
+						{ "label", "label_description", gap = 1 },
+						{ "kind" },
+						{ "source_name" },
+					},
+				},
+			},
+		},
+		sources = {
+			default = { "lsp", "path", "snippets" },
+		},
+		fuzzy = { implementation = "prefer_rust" },
+    signature = { enabled = true },
+	},
 }
