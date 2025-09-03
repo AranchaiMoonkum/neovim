7156c8964e2aa12e0a671cf31e13ee2ed70edffd
Aranchai <moonkum.aranchai@gmail.com>
2025-09-03 18:25:21 +0700

    formatted lsp.lua

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,24 +1,29 @@
+local cmp = require("blink.cmp")
+local capabilities = cmp.get_lsp_capabilities()
+
 vim.diagnostic.config({ virtual_text = true })
 
+-- Show diagnostics if available, otherwise show hover
 vim.keymap.set("n", "K", function()
-  local diagnostics = vim.diagnostic.get(0, { lnum = vim.api.nvim_win_get_cursor(0)[1] - 1 })
+	local line = vim.api.nvim_win_get_cursor(0)[1] - 1
+	if vim.diagnostic.get(0, { lnum = line })[1] then
+		vim.diagnostic.open_float()
+	else
+		vim.lsp.buf.hover()
+	end
+end, { desc = "Hover or show diagnostics" })
 
-  if #diagnostics > 0 then
-    vim.diagnostic.open_float()
-  else
-    vim.lsp.buf.hover()
-  end
-end)
-
+-- Attach LSP keymaps once per buffer
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = args.buf })
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = args.buf })
-  end,
+	callback = function(args)
+		local opts = { buffer = args.buf }
+
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+	end,
 })
 
-local capabilities = require("blink.cmp").get_lsp_capabilities()
-
+-- Apply LSP config with cmp capabilities
 vim.lsp.config("*", {
-  capabilities = capabilities
+	capabilities = capabilities,
 })
